1abebe397afb76920c5a813ddfc372bff408a681
Sagleft <sagleft@inbox.ru>
2024-08-24 11:28:04 +0300

    add yaml tags to config struct

--- v2/internal/utopia/structs.go
+++ v2/internal/utopia/structs.go
@@ -16,15 +16,15 @@ type rateLimiters map[string]*rate.RateLimiter
 
 type Config struct {
 	// required
-	Host   string `json:"host"` // default: 127.0.0.1
-	Token  string `json:"token"`
-	Port   int    `json:"port"`
-	WsPort int    `json:"wsport"`
+	Host   string `json:"host" yaml:"host"` // default: 127.0.0.1
+	Token  string `json:"token" yaml:"token"`
+	Port   int    `json:"port" yaml:"port"`
+	WsPort int    `json:"wsport" yaml:"wsport"`
 
 	// optional
-	Protocol              string      `json:"protocol"` // default: http
-	RequestTimeoutSeconds int         `json:"timeout"`
-	Cb                    LogCallback `json:"-"`
+	Protocol              string      `json:"protocol" yaml:"protocol"` // default: http
+	RequestTimeoutSeconds int         `json:"timeout" yaml:"timeout"`
+	Cb                    LogCallback `json:"-" yaml:"-"`
 }
 
 // query is a filter for API requests
